5dbe2887d373a96828d56944faa618bc452fc427
achialandry <achialands@icloud.com>
2017-10-25 08:37:55 +0300

    Basic Contact store with Laravel and Vue.js, see screenshots, basic create, save, update, delete done with SQLite

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>Contacts Store</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="" id="app">
+          <contacts></contacts>
+      </div>
+    </div>
+    <script src="/js/app.js"></script>
+  </body>
+</html>
